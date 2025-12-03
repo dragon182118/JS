64180902c9a903d274bbd9bb03ad9e4130d43893
Jonas Otacilio <57226202+dragon182118@users.noreply.github.com>
2025-12-03 20:46:52 -0300

    Update factory2.js

--- Função/factory2.js
+++ Função/factory2.js
@@ -8,3 +8,14 @@ function Produtos(...args) {
     return itens
 }
 console.log(Produtos({caneta: 'azul'},{lapis: 'rosa'}))
+
+//correção
+
+function Produtos1 (nome, valor) {
+    return {
+        nome,
+        valor
+    }
+}
+console.log(Produtos1('Notebook', 3199.99))
+console.log(Produtos1('MagicPen', 199.99))
