70eef4d4502739f40d56b60fbca20c3509b535f8
Jonas Otacilio <jonasotacilio93@gmail.com>
2025-12-06 16:49:40 -0300

    Update fnConstructor.js

--- Função/fnConstructor.js
+++ Função/fnConstructor.js
@@ -11,3 +11,13 @@ p1 = new Pessoa('José')
 p1.falar()
 
 // fazer uma constructor de outra forma 
+
+function cPessoa (nome) {
+    this.nome = nome
+    this.falar = function () {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+p3 = new cPessoa('Maria')
+p3.falar()
