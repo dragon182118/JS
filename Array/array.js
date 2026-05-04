ca22ccb3d27258aecde07f56f27f7cffac46615d
Jonas Otacilio <jonasotacilio93@gmail.com>
2026-05-04 20:58:14 -0300

    array

---
+++ Array/array.js
@@ -0,0 +1,17 @@
+let myarray = ['Ana', 'Gabriel', 'Rafaela'];
+
+myarray.push('Jose');
+
+console.log(myarray);
+
+myarray.sort();
+
+console.log(myarray);
+
+myarray.splice(1, 0, 'Evandro', 'Krikor')
+
+console.log(myarray);
+
+myarray.splice(1, 2)
+
+console.log(myarray);
