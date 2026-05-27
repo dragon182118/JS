3a57f3aaf3e1d7832ad02d8255d9ee5e06db498c
Jonas Otacilio <jonasotacilio93@gmail.com>
2026-05-27 20:37:10 -0300

    sim

--- Node/projetoAPIExpress/src/server.js
+++ Node/projetoAPIExpress/src/server.js
@@ -2,6 +2,7 @@ const porta = 3003
 
 const express = require('express')
 const db = require('./database')
+const bodyParser = require('body-parser')
 const app = express()
 
 /*app.get('/produtos', (req, res, next) => {
@@ -17,6 +18,8 @@ const app = express()
     res.send({ nome: 'Notebook', preco: 123.45 }) //Converte automaticamente para json
 })*/
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/produtos', (req, res) => {
     res.send(db.getProdutos()) 
 })
@@ -27,9 +30,23 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos', (req, res, next) => {
     const produto = db.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = db.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = db.excluirProduto(req.params.id)
     res.send(produto)
 })
 
